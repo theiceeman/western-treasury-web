25a550f91a4521621ad4b4b76e6f95cdd1b6e27e
okorieebube <okorieebube1@gmail.com>
2025-05-31 10:17:29 +0100

    fixed

--- src/app/app/transactions/sell/[id]/page.tsx
+++ src/app/app/transactions/sell/[id]/page.tsx
@@ -76,7 +76,7 @@ const Page = ({ params }: { params: { id: string } }) => {
 
   };
 
-  console.log('tokenStandard',tokenStandard);return;
+  // console.log('tokenStandard',tokenStandard);return;
   if (isLoading) {
     return (
       <div className="flex justify-center items-center h-screen">
